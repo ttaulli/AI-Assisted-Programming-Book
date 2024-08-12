5a89ca3ae646af5891b0f4876a3eed56d45b0ea3
ttaulli <ttaulli@gmail.com>
2024-08-12 10:22:09 -0700

    Initial

---
+++ 1-github-copilot-basics/github-copilot.py
@@ -0,0 +1,8 @@
+# Use a comment for coding
+# Write a program that converts temperature from Fahrenheit to Celsius
+
+# Use a comment for coding
+# Write a program that converts temperature from Fahrenheit to Celsius
+
+
+
