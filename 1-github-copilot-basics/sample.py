9139a9e6b2f0b79736d71bf61ac45294c0dff080
ttaulli <ttaulli@gmail.com>
2024-08-21 09:53:30 -0700

    Initial

--- 1-github-copilot-basics/sample.py
+++ 1-github-copilot-basics/sample.py
@@ -11,5 +11,8 @@
 # Insert it into the terminal
 # Prompt:  @terminal What is the command to list the files in a directory?
 
-
+# Help with VSCode
+# @vscode How do I open the Command Palette?
+# @vscode How do I change the theme?
+# @vscode How do I install extensions?
 
