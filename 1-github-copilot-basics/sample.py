da351069fb988d57c43b3d9063ae96f4916d7237
ttaulli <ttaulli@gmail.com>
2024-08-21 09:47:36 -0700

    Initial

---
+++ 1-github-copilot-basics/sample.py
@@ -0,0 +1,15 @@
+# Create code by using comments
+# Comment/Prompt:  Create a Python function that converts fahrenheit to celsius
+
+# Start with def add
+
+# Create code by using Chat
+# Prompt:  Write a program that converts fahrenheit to celsius
+
+# Agents
+# Help with the terminal
+# Insert it into the terminal
+# Prompt:  @terminal What is the command to list the files in a directory?
+
+
+
