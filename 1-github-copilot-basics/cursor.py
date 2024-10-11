6df5a6fb4926b231d7dd0d9f66073583da1d2686
ttaulli <ttaulli@gmail.com>
2024-10-11 08:24:00 -0700

    Refactor code to include features and system prompts for using GitHub Copilot in GitHub.com

--- 1-github-copilot-basics/cursor.py
+++ 1-github-copilot-basics/cursor.py
@@ -1,13 +1,24 @@
 # Using Cursor  
 
-# Settings
-# Import VS Code extensions, settings
-# Rules for AI (all projects)
-# .cursor rules file (one project)
-# Models
+# Features
 
-# autocomplete
-# def 
+# System prompts
+# Context added to the prompt, such as coding conventions you want, language,
+# tech stack, etc.
+# Applied when using chat, tab completion, composer
+
+# Can do this for a project
+# Create .cursorrules file in the root of the project
+# Can find system prompts at cursor.directory
+# Search for a topic, like nextjs
+# Copy and include in .cursorrules file
+# Can use this on a global basis 
+# Click settings button
+# Go to Rules for AI
+
+# Can select different models
+# Can use your own LLM API keys
+
 
 
 
