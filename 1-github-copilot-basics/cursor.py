915494d598f56a3158f6f79ee87dcb167a012848
ttaulli <ttaulli@gmail.com>
2024-10-31 18:50:28 -0700

    Refactor code to improve comments on chat functionality and auto completion in cursor.py

--- 1-github-copilot-basics/cursor.py
+++ 1-github-copilot-basics/cursor.py
@@ -12,46 +12,33 @@
 # Auto completion
 # Use tab to accept
 
-
-
-def 
+def  
 
 # Fix code on the fly
 add_numbers(num1, num2):
 
-
+# in-line chat
+# cmd + k (Mac), ctrl + k (Windows)
+# Can select different models
+# Can use your own LLM API keys
 
 # Chat
 # CMD + L (Mac)
 # CTRL + L (Windows)
 # Asks questions about the code
 # Default is the current screen in the editor
-# Click + to add context
-# Can include images for this
+# Click @ to add context
+# + to add files for the context too
+# can select entire codebase option
 # Select the todo app
 # Prompt:  Can you add a selection for clear?
 # Can copy or apply it to the code
 
-# System prompts
-# Context added to the prompt, such as coding conventions you want, language,
-# tech stack, etc.
-# Applied when using chat, tab completion, composer
-
-# Can do this for a project
-# Create .cursorrules file in the root of the project
-# Can find system prompts at cursor.directory
-# Search for a topic, like nextjs
-# Copy and include in .cursorrules file
-# Can use this on a global basis 
-# Click settings button
-# Go to Rules for AI
-
-# Can select different models
-# Can use your own LLM API keys
-
-# Index code
-# Uses RAG (Retrieval Augmented Generation)
-# Sophisticated search for your codebase
+# Composer
+# cmd+i (Mac), ctrl+i (Windows)
+# Simplify process for multi-file edits/complex edits
+# press the middle button on the right to expand
+# Prompt:  Create a user authentication system for todo-app
 
 
 
