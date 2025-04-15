62bb1517618c16d265cf14eea193d162f1296029
ttaulli <ttaulli@gmail.com>
2025-04-15 13:24:02 -0700

    initial

---
+++ 2-chatgpt-basics/chatgpt.py
@@ -0,0 +1,34 @@
+# Capabilities of ChatGPT
+
+# Usually for popular languages:  Python, JavaScript, Java
+# Popular libraries:  pandas
+# Depends on what the LLM is trained on
+
+# Tour of ChatGPT
+# Keep a session to one topic (click the chat icon at top left of screen)
+# LLM can get confused, misdirected
+# Can search chats
+# Can share, rename, delete and archive chats
+# Can choose the model (top left of screen)
+
+# Customize ChatGPT (click profile at top right)
+# Your background
+# What traits should ChatGPT have?
+
+# “Please provide clear and concise explanations with 
+# code examples whenever possible. Use technical language 
+# appropriate for someone with an intermediate to advanced 
+# understanding of programming. When discussing new concepts 
+# or tools, give a brief overview before diving into more 
+# complex details. Also, feel free to suggest best practices, 
+# optimization tips, or alternative approaches that could 
+# improve my code.”
+
+# Coding prompt:
+
+# Write a Python function that reverses a string.
+
+# Select Edit in Canvas
+
+# Select icon on bottom right for programming options
+
