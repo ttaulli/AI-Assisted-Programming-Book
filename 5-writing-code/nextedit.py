c753b30038fcbb33138b464624130970a5aa710e
ttaulli <ttaulli@gmail.com>
2025-04-27 16:00:02 -0700

    initial

---
+++ 5-writing-code/nextedit.py
@@ -0,0 +1,35 @@
+# Rename pizza to pizzaType.
+
+pizza_type = 'Pepperoni'
+print(pizza_type)
+
+
+# Changing class/method/function names
+# change class to 3DPoint
+
+import math
+
+class Point:
+    def __init__(self, x: float, y: float):
+        self._x = x
+        self._y = y
+
+    def get_distance(self) -> float:
+        return math.sqrt(self._x ** 2 + self._y ** 2)
+    
+# Refractoring
+# Change greet to farewell
+
+# Function with Default Parameters
+
+def multiply(a: float, b: float = 1) -> float:
+    return a * b
+
+# Function with Optional Parameters
+def greet(name: str, greeting: str = None) -> str:
+    return f"{greeting or 'Hello'}, {name}!"
+
+# Fibonacci function
+def fib(n: int) -> int:
+    return n if n <= 1 else fib(n - 1) + fib(n - 2)
+
